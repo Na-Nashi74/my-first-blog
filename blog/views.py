6116c08cc7bfda1ae12f40e256cd83926a107aa2
Na-Nashi74 <nashinoki73@gmail.com>
2025-05-05 18:45:36 +0900

    Added view and template for detailed blog post as well as CSS for the site.

--- blog/views.py
+++ blog/views.py
@@ -7,6 +7,8 @@ from django.utils import timezone
 # Postモデルをインポート（ブログの記事データを扱う）
 from .models import Post  
 
+from django.shortcuts import render, get_object_or_404
+
 # 記事一覧を表示するビュー関数
 def post_list(request):  
     # 現在時刻より前に公開された記事を取得し、公開日順に並べる
@@ -14,3 +16,8 @@ def post_list(request):
     
     # 'blog/post_list.html' のテンプレートをレンダリングし、postsのデータを渡す
     return render(request, 'blog/post_list.html', {'posts': posts})  
+
+#投稿の詳細を表示するビュー関数
+def post_detail(request, pk):
+    post = get_object_or_404(Post, pk=pk)
+    return render(request, 'blog/post_detail.html', {'post': post})
